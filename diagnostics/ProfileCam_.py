e68bc830846639d3a7410173433c7273e0f2dbf6
Bryn Lloyd <bryn.lloyd@jesus.ox.ac.uk>
2026-02-06 17:24:23 +0100

    can load in HRM4 image. currently ignoring calibration (just stole HRM6 calib, but pass calib_id=None when calling super().get_proc_shot())

---
+++ diagnostics/ProfileCam_.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from scipy.interpolate import interp1d
+from scipy.signal import savgol_filter
+import re
+
+from LAMP.diagnostic import Diagnostic
+from LAMP.utils.image_proc import ImageProc
+from LAMP.utils.general import dict_update, mindex
+from LAMP.utils.plotting import *
+
+class ProfileCam_(Diagnostic):
+    """
+    Profile cams are HRM3 and HRM4
+    """
+
+    __version = 0.1
+    __authors = ['Brendan Kettle', "Bryn Lloyd"]
+    __requirements = 'cv2'
+    data_type = 'csv'
+
+    curr_img = None
+    img_units = ['Counts']
+    x_mm, y_mm = None, None
+    x_mrad, y_mrad = None, None
+    x_MeV, y_MeV = None, None
+
+    def __init__(self, exp_obj, config_filepath):
+        """Initiate parent base Diagnostic class to get all shared attributes and funcs"""
+        super().__init__(exp_obj, config_filepath)
+        return
+
+    def get_proc_shot(self, shot_dict, calib_id=None, apply_disp=True, apply_div=True, apply_charge=True, roi_mm=None, roi_MeV=None, roi_mrad=None, debug=False):
+        """
+        Return a processed shot using saved or passed calibrations. (there is no calibration for this for now)
+        Wraps base diagnostic class function, adding dispersion, divergence, charge.
+        """
+
+        # use diagnostic base function
+        # loads calib id and run_img_calib for standard calibration routines
+        # img, x, y = super().get_proc_shot(shot_dict, calib_id=calib_id, debug=debug)
+        img, x, y = super().get_proc_shot(shot_dict, calib_id=None, debug=debug)
+        if img is None:
+            return None, None, None
+
+        return img, x, y
