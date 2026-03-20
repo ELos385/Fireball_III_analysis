992dea61aa07c3e6931b8e2cebc966750f9bba01
Maximilian Mudra <m.mudra@unibw.de>
2026-03-20 11:07:58 +0100

    Created basic orca diagnostic

---
+++ diagnostics/Orca.py
@@ -0,0 +1,13 @@
+from LAMP.diagnostic import Diagnostic
+
+class Orca(Diagnostic):
+    """Orca diagnostic class. This can be used as a starting point for creating
+    new diagnostics. It inherits from the base Diagnostic class, which provides shared attributes
+    and functions for all diagnostics.
+    """
+
+    def __init__(self, exp_obj, config_filepath):
+        """Initiate parent base Diagnostic class to get all shared attributes and funcs"""
+        self.data_type = config_filepath['data_type'] # This is needed for __init__ of Diagnostic
+        super().__init__(exp_obj, config_filepath)
+        return
